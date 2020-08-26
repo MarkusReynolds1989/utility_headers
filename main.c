db36fd931c4f7db23d22d03803dc018177f15a20
Markus <markusreynolds1989@gmail.com>
2020-08-25 20:05:15 -0500

    Realloc smaller

--- main.c
+++ main.c
@@ -17,9 +17,14 @@ array_list* new_array_list(int size) {
     return output;
 }
 
+void array_list_free(array_list* list) {
+    free(list->array);
+    free(list);
+}
+
 void array_list_add(array_list* list, int input) 
 {
-    if(list->count <= list->size + 1){
+    if(list->count <= list->size){
 	list->array[list->count] = input;
     } else {
 	list->array = realloc(list->array, sizeof(int*) * list->size + 1);
@@ -30,21 +35,49 @@ void array_list_add(array_list* list, int input)
     list->count += 1;
     printf("Size: %i\n", list->size);
     printf("Count: %i\n", list->count);
-    printf("Memory: %zu\n", sizeof(list->array) * sizeof(int));
+    printf("Memory: %zu Bytes\n", sizeof(list->array) * sizeof(int) * list->size);
+}
+
+void array_list_remove(array_list* list, int index) 
+{
+    if(index >= list->size) {
+	int* temp = (int*)calloc(list->size, sizeof(int));
+	if(index == 0) {
+	list->array = realloc(list->array, sizeof(int*) * list->size);
+	   for(int i = 0, y = 1; i < list->size; i++, y++) {
+		temp[y] = list->array[i]; 
+	   }
+	list->array = temp;
+	}
+	free(temp);
+    }
+
+    list->count -= 1;
+    list->size -= 1;
+    
+    printf("Size: %i\n", list->size);
+    printf("Count: %i\n", list->count);
+    printf("Memory: %zu Bytes\n", sizeof(list->array) * sizeof(int) * list->size);
+
 }
 
 int main(void) 
 {
     array_list* array1 = new_array_list(5);
     
-    for(int i = 0; i < 200; i++) {
+    for(int i = 0; i < 10; i++) {
 	array_list_add(array1, i + 1);
     }
-
-    for(int i = 0; i < array1->count; i++) {
-	printf("%i\n", array1->array[i]);
+     
+    array_list_remove(array1, 0);
+    array_list_remove(array1, 0);
+    for(int i = 0; i < array1->count; i++ ) {
+	printf("%i", array1->array[i]);
     }
-    free(array1);
+    /*for(int i = 0; i < array1->count; i++) {
+	printf("%i\n", array1->array[i]);
+    }*/
+    array_list_free(array1);
 }
 
 
